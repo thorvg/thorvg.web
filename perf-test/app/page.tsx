524de5134ea3781ccc37bbfc5b54ed4aac75d48d
Hermet Park <hermet@lottiefiles.com>
2024-05-21 17:14:47 +0900

    perf-test: disable testing options.
    these numbers are too harsh for testing now,
    we must apply this after applying the deferred loading.

--- perf-test/app/page.tsx
+++ perf-test/app/page.tsx
@@ -113,9 +113,9 @@ const countOptions = [
   { id: 1, name: 20 },
   { id: 2, name: 50 },
   { id: 3, name: 100 },
-  { id: 4, name: 200 },
-  { id: 5, name: 500 },
-  { id: 6, name: 1000 },
+  //{ id: 4, name: 200 },
+  //{ id: 5, name: 500 },
+  //{ id: 6, name: 1000 },
 ];
 
 const playerOptions = [
