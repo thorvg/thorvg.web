974ff020adec2eb5ffa4a7f7778ca51a181ec547
Jinny You <jinny@lottiefiles.com>
2024-06-13 20:59:21 +0900

    perf-test: Added skottie player

--- perf-test/next.config.mjs
+++ perf-test/next.config.mjs
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (typeof nextRuntime === "undefined") {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+
     config.module.rules.push({
       test: /\.wasm$/,
       loader: "file-loader",
       options: {
         publicPath: "/_next/static/wasm/",
         outputPath: "static/wasm/",
-        name: "[name].[contenthash].[ext]"
+        name: "[name].[contenthash].[ext]",
       },
     });
 
