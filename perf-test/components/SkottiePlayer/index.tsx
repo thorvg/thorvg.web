c1ee3518f057c30bc6f36642a98b33005000f00b
Jinny You <jinny@lottiefiles.com>
2024-07-05 14:56:28 +0800

    perf-test: Support DPR with SkottiePlayer

--- perf-test/components/SkottiePlayer/index.tsx
+++ perf-test/components/SkottiePlayer/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { CanvasKit, Surface } from "canvaskit-wasm";
+import { useEffect, useRef, useState } from "react";
+import { CanvasKit } from "canvaskit-wasm";
 import { v4 as uuidv4 } from 'uuid';
 
 let canvasKit: CanvasKit | undefined = undefined;
@@ -17,6 +17,7 @@ interface Props {
 }
 
 export default function Skottie ({ lottieURL, width, height }: Props) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [id, setId] = useState<string>('');
   let initialized = false;
 
@@ -33,27 +34,23 @@ export default function Skottie ({ lottieURL, width, height }: Props) {
     const id = uuidv4();
     setId(id);
 
+    const dpr = window.devicePixelRatio || 1;
+
     const data = await fetch(lottieURL);
     const lottieJSON = await data.text();
 
     const animation = canvasKit.MakeManagedAnimation(lottieJSON);
-    const MAX_FRAMES = animation.duration() * animation.fps();
-    let surface: Surface | null;
-
-    surface = canvasKit.MakeSWCanvasSurface(id);
-    if (!surface) {
-      throw new Error('[Skia] Failed to create canvas surface');
-    }
-
+    const bounds = canvasKit!.LTRBRect(0, 0, width * dpr, height * dpr);
+    canvasRef.current!.width = width * dpr;
+    canvasRef.current!.height = height * dpr;
+    
     let beginTime = Date.now() / 1000;
+    let surface = canvasKit!.MakeSWCanvasSurface(id);
+    let canvas = surface!.getCanvas();
 
-    const canvas = surface.getCanvas();
     const damageRect = Float32Array.of(0, 0, 0, 0);
-    const bounds = canvasKit.LTRBRect(0, 0, width, height);
     const clearColor = canvasKit.TRANSPARENT;
-
     function drawFrame() {
-
       let currentTime = Date.now() / 1000;
       let currentFrame = (currentTime - beginTime) / animation.duration();
 
@@ -75,6 +72,6 @@ export default function Skottie ({ lottieURL, width, height }: Props) {
   }
 
   return (
-    <canvas id={id} width={width} height={height} />
+    <canvas id={id} ref={canvasRef} style={{ width, height }} />
   );
 }
