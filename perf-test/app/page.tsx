3a1067e4859e67d9fa7be20060f9de0471946adf
Jinny You <jinny@lottiefiles.com>
2025-06-14 13:55:44 +0900

    perf-test: hotfix WebGPU rendering issue
    Manually updated WASM to add hotfixed binary with v1.0-pre22 for WebGPU issue.
    
    This will be reverted in next release.

--- perf-test/app/page.tsx
+++ perf-test/app/page.tsx
@@ -11,7 +11,8 @@ import dotLottieWasmUrl from "../node_modules/@lottiefiles/dotlottie-web/dist/do
 import SkottiePlayer, { setCanvasKit } from '../components/SkottiePlayer';
 import skottieWasmUrl from "../node_modules/canvaskit-wasm/bin/full/canvaskit.wasm";
 import InitCanvasKit from 'canvaskit-wasm/bin/full/canvaskit';
-import wasmUrl from "../node_modules/@thorvg/lottie-player/dist/thorvg-wasm.wasm";
+// import wasmUrl from "../node_modules/@thorvg/lottie-player/dist/thorvg-wasm.wasm";
+const wasmUrl = '/thorvg-wasm.wasm';
 
 setDotLottieWasmUrl(dotLottieWasmUrl);
 
@@ -165,7 +166,7 @@ export default function Home() {
     initialized = true;
 
     // @ts-ignore
-    import("@thorvg/lottie-player");
+    import("/public/lottie-player.js");
 
     let count: number = countOptions[1].name;
     let seed: string = '';
