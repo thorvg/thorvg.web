b0cab74f1b9ac9812b39206365f3c1643c977bd7
lerrybe <yeji.kim@dalpha.so>
2025-09-06 15:57:36 +0900

    examples: Updated the example to React with Vite

--- example/react/src/main.tsx
+++ example/react/src/main.tsx
@@ -4,6 +4,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
 const root = createRoot(rootElement);
 
 root.render(
