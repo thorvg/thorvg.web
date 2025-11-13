5d55c6bbcddbb62a14c28f89ab25c898a167dc25
Jinny You <jinny@lottiefiles.com>
2025-11-14 03:02:44 +0900

    build: migrate to pnpm monorepo structure
    - Restructure project as pnpm workspace for upcoming multi-package support
    - Replace yarn with pnpm for consistent dependency management
    
    issue: https://github.com/thorvg/thorvg.web/issues/17

--- packages/lottie-player/wasm_setup.sh
+++ packages/lottie-player/wasm_setup.sh
@@ -3,20 +3,20 @@
 echo "EMSDK: $EMSDK"
 
 rm -rf build_wasm && sh ./wasm_build.sh $EMSDK/
-mv build_wasm/thorvg.{wasm,js,d.ts} ./dist
+mv ../../wasm/lottie-player/build_wasm/thorvg.{wasm,js,d.ts} ./dist
 
 rm -rf build_wasm && sh ./wasm_build.sh sw $EMSDK/
 mkdir -p ./dist/sw
-mv build_wasm/thorvg.{wasm,js} ./dist/sw
+mv ../../wasm/lottie-player/build_wasm/thorvg.{wasm,js} ./dist/sw
 
 rm -rf build_wasm && sh ./wasm_build.sh gl $EMSDK/
 mkdir -p ./dist/gl
-mv build_wasm/thorvg.{wasm,js} ./dist/gl
+mv ../../wasm/lottie-player/build_wasm/thorvg.{wasm,js} ./dist/gl
 
 rm -rf build_wasm && sh ./wasm_build.sh sw-lite $EMSDK/
 mkdir -p ./dist/sw-lite
-mv build_wasm/thorvg.{wasm,js} ./dist/sw-lite
+mv ../../wasm/lottie-player/build_wasm/thorvg.{wasm,js} ./dist/sw-lite
 
 rm -rf build_wasm && sh ./wasm_build.sh gl-lite $EMSDK/
 mkdir -p ./dist/gl-lite
-mv build_wasm/thorvg.{wasm,js} ./dist/gl-lite
+mv ../../wasm/lottie-player/build_wasm/thorvg.{wasm,js} ./dist/gl-lite
