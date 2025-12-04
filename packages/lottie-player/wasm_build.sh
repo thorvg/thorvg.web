764a217e456e92e3c09b591fe27a2ef800b483b8
Jinny You <jinny@lottiefiles.com>
2025-12-04 16:50:11 +0900

    Restructuring

--- packages/lottie-player/wasm_build.sh
+++ packages/lottie-player/wasm_build.sh
@@ -8,30 +8,41 @@ if [ -z "$2" ]; then
   EMSDK="$1"
 fi
 
-cd thorvg
+# Step 1: Build ThorVG library
+cd ../../thorvg
 rm -rf build_wasm
 
 if [[ "$BACKEND" == "wg" ]]; then
   sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_wg.txt > /tmp/.wasm_cross.txt
-  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie, jpg, png, webp, ttf" -Dthreads=false -Dfile="false" -Dbindings="wasm_beta" -Dpartial=false -Dengines="wg" --cross-file /tmp/.wasm_cross.txt build_wasm
+  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie,jpg,png,webp,ttf" -Dthreads=false -Dfile="false" -Dpartial=false -Dengines="wg" --cross-file /tmp/.wasm_cross.txt build_wasm
 elif [[ "$BACKEND" == "sw" ]]; then
   sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_sw.txt > /tmp/.wasm_cross.txt
-  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie, jpg, png, webp, ttf" -Dthreads=false -Dbindings="wasm_beta" -Dpartial=false -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
+  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie,jpg,png,webp,ttf" -Dthreads=false -Dpartial=false -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
 elif [[ "$BACKEND" == "gl" ]]; then
   sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_gl.txt > /tmp/.wasm_cross.txt
-  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie, jpg, png, webp, ttf" -Dthreads=false -Dbindings="wasm_beta" -Dpartial=false -Dengines="gl" -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
+  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie,jpg,png,webp,ttf" -Dthreads=false -Dpartial=false -Dengines="gl" -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
 elif [[ "$BACKEND" == "sw-lite" ]]; then
   sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_sw.txt > /tmp/.wasm_cross.txt
-  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie, png" -Dextra="" -Dthreads=false -Dbindings="wasm_beta" -Dpartial=false -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
+  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie,png" -Dextra="" -Dthreads=false -Dpartial=false -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
 elif [[ "$BACKEND" == "gl-lite" ]]; then
   sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_gl.txt > /tmp/.wasm_cross.txt
-  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie, png" -Dextra="" -Dthreads=false -Dbindings="wasm_beta" -Dpartial=false -Dengines="gl" -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
+  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="lottie,png" -Dextra="" -Dthreads=false -Dpartial=false -Dengines="gl" -Dfile="false" --cross-file /tmp/.wasm_cross.txt build_wasm
 else
   sed "s|EMSDK:|$EMSDK|g; s|'--bind'|'--bind', '--emit-tsd=thorvg.d.ts'|g" ./cross/wasm32.txt > /tmp/.wasm_cross.txt
-  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dthreads=false -Dbindings="wasm_beta" -Dpartial=false -Dengines="all" --cross-file /tmp/.wasm_cross.txt build_wasm
+  meson setup -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dthreads=false -Dpartial=false -Dengines="all" --cross-file /tmp/.wasm_cross.txt build_wasm
 fi
 
 ninja -C build_wasm/
-ls -lrt build_wasm/src/bindings/wasm/*.{js,wasm}
 
-cd ..
+cd ../packages/lottie-player
+
+# Step 2: Build WASM bindings
+rm -rf build_wasm
+
+cp ../../thorvg/build_wasm/config.h ../../bindings/lottie/config.h
+meson setup -Db_lto=true --cross-file /tmp/.wasm_cross.txt build_wasm ../../bindings/lottie
+
+ninja -C build_wasm/
+rm ../../bindings/lottie/config.h
+
+ls -lrt build_wasm/*.{js,wasm}
