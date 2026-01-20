5cec482fe9ffce69bef079c1a890f7720373991d
Jinny You <jinny@lottiefiles.com>
2026-01-20 17:13:05 +0900

    SharedArrayBuffer header for playgroud

--- playground/next.config.mjs
+++ playground/next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config, { isServer }) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
 
@@ -21,6 +38,7 @@ const nextConfig = {
       module: false,
       fs: false,
       path: false,
+      worker_threads: false,
     };
 
     return config;
